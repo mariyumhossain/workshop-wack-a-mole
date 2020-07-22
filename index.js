0c881a9d5354cec15c94a0270e0cb5e7a420e47e
Mariyum Hossain <mariyumhossain@Mariyums-Air.lan1>
2020-07-22 17:52:05 -0400

    worked on JS file

--- index.js
+++ index.js
@@ -2,5 +2,26 @@
 
 let score = 0;
 var scoreDisplay = document.getElementById('score');
+var gameSquare = document.getElementById("whack-a-mole")
+
 
 console.log(scoreDisplay);
+
+var hole = document.getElementsByClassName("hole")
+
+var length = hole.length
+
+
+setInterval(function(){
+    var randomHoleNum = Math.floor(Math.random() * Math.floor(length))
+    hole[randomHoleNum].classList.toggle("mole")
+
+},200)
+
+gameSquare.addEventListener("click",function(click){
+    if (click.target.matches(".mole")) { 
+        score++
+        scoreDisplay.innerText = score
+        click.target.classList.toggle('mole')
+    }
+})
